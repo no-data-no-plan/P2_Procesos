3a889a9b6c8e56bb1fff1c444caf9da665264dbc
Bia_Caravaca <mabi24@bemen3.cat>
2025-11-04 15:50:37 +0100

    Añadido el ejercicio 1 que por algo se perdió

--- src/Exercici1/Exercici1/Asiento.java
+++ src/Exercici1/Exercici1/Asiento.java
@@ -1,4 +1,13 @@
 package Exercici1.Exercici1;
 
 public class Asiento {
+    private int idAsiento;
+
+    public Asiento(int idAsiento) {
+        this.idAsiento = idAsiento;
+    }
+
+    public int getIdAsiento() {
+        return this.idAsiento;
+    }
 }
--- src/Exercici1/Exercici1/Main.java
+++ src/Exercici1/Exercici1/Main.java
@@ -1,1 +1,13 @@
-package Exercici1;
+package Exercici1.Exercici1;
+
+public class Main {
+    public static void main(String[] args) {
+        Amfiteatre Amfi = new Amfiteatre();
+        Amfi.setAsientosAmfiteatre(Amfi.asientosAmfiteatre);
+
+        for(Asiento asiento : Amfi.getAsientosAmfiteatre()) {
+            System.out.println(asiento.getIdAsiento());
+        }
+
+    }
+}
